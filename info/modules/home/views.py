9320c12d2646ad05ea865434502303a9a33598db
zf <zhangf1993@126.com>
2018-09-25 20:27:17 +0800

    首页渲染

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,19 +1,15 @@
 import logging
 
-from flask import session, current_app
+from flask import session, current_app, render_template
 
 from . import home_blu
 
 
 @home_blu.route('/')
 def index():
-    session['name'] = 'zs'
-    # logging.error('抛出异常！')
-    current_app.logger.error('抛出异常！')
-    return 'index'
+    return render_template('index.html')
 
 
 @home_blu.route('/favicon.ico')
 def favicon():
-    print('111')
     return current_app.send_static_file('news/images/favicon.ico')
