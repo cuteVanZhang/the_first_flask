f565edce26a1685e9587a552dac43ae73c8319cd
zf <zhangf1993@126.com>
2018-09-25 20:16:32 +0800

    添加favicon.ico图标

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -11,3 +11,9 @@ def index():
     # logging.error('抛出异常！')
     current_app.logger.error('抛出异常！')
     return 'index'
+
+
+@home_blu.route('/favicon.ico')
+def favicon():
+    print('111')
+    return current_app.send_static_file('news/images/favicon.ico')
