0a99ef8e411f5bbb474b4f7b7b4c54a39f2867f5
zf <zhangf1993@126.com>
2018-10-08 17:36:32 +0800

    修改密码

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -17,7 +17,7 @@ def user_info():
     return render_template("user.html", user=user.to_dict())
 
 
-# 基本信息
+# 基本资料
 @user_blu.route('/base_info', methods=["POST", "GET"])
 @user_login_data
 def base_info():
@@ -58,3 +58,36 @@ def base_info():
 
     return jsonify(errno=RET.OK, errmsg=error_map[RET.OK])
 
+
+# 密码修改
+@user_blu.route('/pass_info', methods=["POST", "GET"])
+@user_login_data
+def pass_info():
+
+    user = g.user
+    if not user:
+        return redirect(url_for("home.index"))
+
+    if request.method == "GET":
+        return render_template("user_pass_info.html")
+
+    # 点击保存，提交post请求
+    # 获取校验参数
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 密码长度不能少于6位
+    if len(new_password) < 6:
+        return jsonify(errno=RET.PARAMERR, errmsg="密码长度不能少于6位")
+
+    # 校验原密码是否正确
+    if not user.check_password(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg=error_map[RET.PWDERR])
+
+    # 操作数据库数据
+    user.password = new_password
+
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK])
